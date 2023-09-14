80895c7b86316f8ac835b6f558560231e639ad14
ibrahimovihor <ibrahimovihor@gmail.com>
2023-09-15 01:57:46 +0300

    Updates puppeteer config file

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,9 +1,10 @@
 const { join } = require('path')
+const os = require('os')
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
+  cacheDirectory: join(os.homedir(), '.cache', 'puppeteer')
 }
